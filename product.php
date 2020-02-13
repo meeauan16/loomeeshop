b6986894b591a7672c5a7ebeb5be5a1c8c334f3f
asjonat <banmaekampong@gmail.com>
2020-02-14 01:22:52 +0700

    hi

--- product.php
+++ product.php
@@ -31,31 +31,221 @@
 	
 		<div class="container">
 		<h3>สินค้า</h3>
-			<section>
-				<h4>ตุ๊กตา</h4>
-				<hr>
-				<form method="GET" action="product.php" align="right">
+		<form method="GET" action="product.php" align="right">
 					<input type="text" name="search"  placeholder="กรอกชื่อสินค้า" style="width: 20%" id="input">
 					<input type="submit" class="btn btn-primary" value="ค้นหา">
 					<input type="reset" class="btn btn-primary" value="ค้นหา">
-				</form>
-
+		</form>
+			<section id="1">
+				<h4>ตุ๊กตา</h4>
+				<hr>
 				<?php foreach($query as $result){ ?>
-								<form action="details.php" method="get">
-									<div class="col-sm-3" style="padding-top: 20px;">
-										<div class="card" style="width: 18rem;">
-											<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
-											<div class="card-body">
-											<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-											ราคา : <?php echo $result['price'] ?></p>
-											<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
-											<input type="submit" class="btn btn-success" value="เพิ่มเติม">
-											</div>
-										</div>
-									</div>
-								</form>
-							<?php } ?> 
-							<hr>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="2">
+				<h4>พวงกุญแจ</h4>
+				<?php
+					$sql = "select * from product where type = 'พวงกุญแจ";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="3">
+				<h4>กระเป๋า</h4>
+				<?php
+					$sql = "select * from product where type = 'กระเป๋า";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="4">
+				<h4>หมอนผ้าห่ม</h4>
+				<?php
+					$sql = "select * from product where type = 'หมอนผ้าห่ม";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="5">
+				<h4>โคมไฟ</h4>
+				<?php
+					$sql = "select * from product where type = 'โคมไฟ";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="6">
+				<h4>ถุงเท้า</h4>
+				<?php
+					$sql = "select * from product where type = 'ถุงเท้า";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="7">
+				<h4>แก้ว</h4>
+				<?php
+					$sql = "select * from product where type = 'แก้ว";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="8">
+				<h4>เข็มกลัด</h4>
+				<?php
+					$sql = "select * from product where type = 'เข็มกลัด";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
+			</section>
+
+			<section id="9">
+				<h4>โคมไฟ</h4>
+				<?php
+					$sql = "select * from product where type = 'รองเท้า";
+					$query = mysqli_query($conn,$sql);
+				?>
+				<hr>
+				<?php foreach($query as $result){ ?>
+					<form action="details.php" method="get">
+						<div class="col-sm-3" style="padding-top: 20px;">
+							<div class="card" style="width: 18rem;">
+								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<div class="card-body">
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
+								ราคา : <?php echo $result['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
+								</div>
+							</div>
+						</div>
+					</form>
+				<?php } ?> 
 			</section>
 		</div>
     </body>
