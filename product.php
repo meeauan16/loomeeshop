a043d1767e4801004e161e7e3f699914915714d3
asjonat <banmaekampong@gmail.com>
2020-02-14 01:36:21 +0700

    commit

--- product.php
+++ product.php
@@ -83,15 +83,15 @@
 			<section id="2">
 				<h4>พวงกุญแจ</h4>
 				<hr>
-				<?php foreach($query1 as $result){ ?>
+				<?php foreach($query1 as $result1){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result1['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result1['brand'] ?>&nbsp; <?php echo $result1['pro_name'] ?><br>
+								ราคา : <?php echo $result1['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result111['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -103,15 +103,15 @@
 			<section id="3">
 				<h4>กระเป๋า</h4>
 				<hr>
-				<?php foreach($query2 as $result){ ?>
+				<?php foreach($query2 as $result2){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result2['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result2['brand'] ?>&nbsp; <?php echo $result2['pro_name'] ?><br>
+								ราคา : <?php echo $result2['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result3['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -123,15 +123,15 @@
 			<section id="4">
 				<h4>หมอนผ้าห่ม</h4>
 				<hr>
-				<?php foreach($query3 as $result){ ?>
+				<?php foreach($query3 as $result3){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result3['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result3['brand'] ?>&nbsp; <?php echo $result3['pro_name'] ?><br>
+								ราคา : <?php echo $result3['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result3['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -143,15 +143,15 @@
 			<section id="5">
 				<h4>โคมไฟ</h4>
 				<hr>
-				<?php foreach($query4 as $result){ ?>
+				<?php foreach($query4 as $result4){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result4['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result4['brand'] ?>&nbsp; <?php echo $result4['pro_name'] ?><br>
+								ราคา : <?php echo $result4['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result4['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -163,15 +163,15 @@
 			<section id="6">
 				<h4>ถุงเท้า</h4>
 				<hr>
-				<?php foreach($query5 as $result){ ?>
+				<?php foreach($query5 as $result5){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result5['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result5['brand'] ?>&nbsp; <?php echo $result5['pro_name'] ?><br>
+								ราคา : <?php echo $result5['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result5['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -183,15 +183,15 @@
 			<section id="7">
 				<h4>แก้ว</h4>
 				<hr>
-				<?php foreach($query6 as $result){ ?>
+				<?php foreach($query6 as $result6){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result6['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result6['brand'] ?>&nbsp; <?php echo $result6['pro_name'] ?><br>
+								ราคา : <?php echo $result6['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result6['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -203,15 +203,15 @@
 			<section id="8">
 				<h4>เข็มกลัด</h4>
 				<hr>
-				<?php foreach($query7 as $result){ ?>
+				<?php foreach($query7 as $result7){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result7['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result7['brand'] ?>&nbsp; <?php echo $result7['pro_name'] ?><br>
+								ราคา : <?php echo $result7['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result7['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
@@ -223,15 +223,15 @@
 			<section id="9">
 				<h4>รองเท้า</h4>
 				<hr>
-				<?php foreach($query9 as $result){ ?>
+				<?php foreach($query9 as $result8){ ?>
 					<form action="details.php" method="get">
 						<div class="col-sm-3" style="padding-top: 20px;">
 							<div class="card" style="width: 18rem;">
-								<img src="pic/<?php echo $result['picture'] ?> " class="card-img-top" style="width: 150px ;">
+								<img src="pic/<?php echo $result8['picture'] ?> " class="card-img-top" style="width: 150px ;">
 								<div class="card-body">
-								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result['pro_name'] ?><br>
-								ราคา : <?php echo $result['price'] ?></p>
-								<input type="hidden" name="id" value="<?php echo $result['pro_id'] ?>"> 
+								<p class="card-text"><?php echo $result['brand'] ?>&nbsp; <?php echo $result8['pro_name'] ?><br>
+								ราคา : <?php echo $result8['price'] ?></p>
+								<input type="hidden" name="id" value="<?php echo $result8['pro_id'] ?>"> 
 								<input type="submit" class="btn btn-success" value="เพิ่มเติม">
 								</div>
 							</div>
